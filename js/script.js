0e14c075acb102848d6795d4fc9f6fa54e71f2e5
adrianopiacentini <adrianopiacentini03@gmail.com>
2024-11-25 21:54:21 +0100

    Recupero esercizio mercoledì 13

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const cardContainer = document.getElementById('card-container')
-// console.log(cardContainer)
-
+const overlay = document.getElementById('overlay')
+const overlayImg = document.getElementById('overlay-img')
+const overlayBtn = document.getElementById('overlay-btn')
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
-.then ((resp) => {
-    resp.data.forEach(element => {
-        cardContainer.innerHTML += 
-        `
+    .then((resp) => {
+        resp.data.forEach(element => {
+            cardContainer.innerHTML +=
+                `
          <div class="card d-flex">
            <img src="./img/pin.svg" class="pin" alt="">
             <div class="card-img mt-3">
@@ -14,5 +15,18 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
             <div class="card-text">${element.title}</div>
          </div>
         `
+        })
+        const cards = document.querySelectorAll('.card')
+        cards.forEach((curCard) => {
+            curCard.addEventListener('click', () => {
+                overlay.classList.remove('d-none')
+                const img = curCard.querySelector('.card-img img')
+                overlayImg.src = img.src
+            })
+        })
+
     })
+
+overlayBtn.addEventListener('click', () => {
+    overlay.classList.add('d-none')
 })
